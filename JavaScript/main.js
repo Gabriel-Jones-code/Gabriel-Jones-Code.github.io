b655f8048a5dca09426cf3a0b6b5bb007f3e77ae
Gabriel <GABEHJ2001@GMAIL.COM>
2022-01-30 16:00:00 -0800

    General Update

---
+++ JavaScript/main.js
@@ -0,0 +1,9 @@
+function opens() {
+    document.getElementById('cont').style.display = 'block';
+    document.getElementById('open').style.display = 'none';
+}
+
+function closing() {
+    document.getElementById('cont').style.display = 'none';
+    document.getElementById('open').style.display = 'block';
+}
