270ed2fc37dcfbd46f622bd1ec04eeb753e20d00
Gabe <GABEHJ2001@GMAIL.COM>
2022-01-31 01:29:23 -0800

    general update

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -7,3 +7,24 @@ function closing() {
     document.getElementById('cont').style.display = 'none';
     document.getElementById('open').style.display = 'block';
 }
+
+function submits() {
+    let first = document.forms['cArray']['firstName'].value;
+    let last = document.forms['cArray']['lastName'].value;
+    let email = document.forms['cArray']['email'].value;
+    if (first == ''){
+        alert('Please provide a first name');
+    }
+
+    else if (last == ''){
+        alert('Please provide a last name');
+    }
+
+    else if (!email.includes('@')){
+        alert('Please provide a valid email');
+    }
+
+    else {
+        alert('Submited');
+    }
+}
