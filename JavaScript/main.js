d1055268e5ac65a42edf5a504e9183706a49a6a8
Gabriel <GABEHJ2001@GMAIL.COM>
2022-04-20 21:31:54 -0700

    insta

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,30 +1,4 @@
-function opens() {
-    document.getElementById('cont').style.display = 'block';
-    document.getElementById('open').style.display = 'none';
-}
-
-function closing() {
-    document.getElementById('cont').style.display = 'none';
-    document.getElementById('open').style.display = 'block';
-}
-
-function submits() {
-    let first = document.forms['cArray']['firstName'].value;
-    let last = document.forms['cArray']['lastName'].value;
-    let email = document.forms['cArray']['email'].value;
-    if (first == ''){
-        alert('Please provide a first name');
-    }
-
-    else if (last == ''){
-        alert('Please provide a last name');
-    }
-
-    else if (!email.includes('@')){
-        alert('Please provide a valid email');
-    }
-
-    else {
-        alert('Submited');
-    }
+function home() {
+    document.getElementById('home')
+    document.scrollIntoView({ behavior: 'smooth' })
 }
