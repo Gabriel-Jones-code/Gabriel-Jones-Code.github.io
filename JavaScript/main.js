6e50bc92c67ebf58d4adee90f93e489e0129aa5f
Gabriel <gabrieljones2001@outlook.com>
2022-09-01 05:28:13 -0700

    General remaster
    I did a lot here. I got the website to hopefully look  a little more professional and got some JS working in the website.
    This, by no means is the final design as I do need to fix the bottom the page, as well as adding more content and moving a couple things around such as the task bar. I also still need to code phone support

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,4 +1,25 @@
-function home() {
-    document.getElementById('home')
-    document.scrollIntoView({ behavior: 'smooth' })
-}
+$(document).ready(function(){      //this is going to put some of school to the test. I'm a bit rusty on jQuery so This is a process
+                                //Basic syntax is $(selector).action().     # selects id and . selects class
+
+    //fade in text
+    var div = $(".about");
+    var text = $(".text")
+    var pos = div.position();
+
+    $(window).scroll(function () {
+        var winpos = $(window).scrollTop();
+        if (winpos >= (pos.top - 650)) {
+            text.addClass("showText");
+        }
+    });
+    //This starts a function when the about portion of the website is ready
+    //I started to assign everything to a var to make things a little easier on myself
+    //I started by looking for when the user scrolls. When a user does end up scrolling,
+    //it'll get the position of the scroll bar. I then started to compare the position to where I want
+    //the event to be triggered. After it reaches the point in which I want the text to show up, 
+    //jQuery changes the text class to the showText class which now shows the contents of the about
+    //section
+
+
+});
+
